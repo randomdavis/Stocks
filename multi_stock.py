85817031fdb24136a663a990e3340b199fd69204
Random Davis <random.davis@gmail.com>
2023-05-22 20:52:43 -0700

    Numerous improvements

--- multi_stock.py
+++ multi_stock.py
@@ -10,7 +10,6 @@ class Stock:
         self.name = name
         self.prices = None
         self.parameters = (S0, mu, sigma, T, dt)
-        self.reset_prices()
 
     def reset_prices(self):
         self.prices = self.geometric_brownian_motion(*self.parameters)
@@ -45,18 +44,20 @@ class Portfolio:
 
 
 class Investor:
-    def __init__(self, portfolio: Portfolio, sell_threshold: float, buy_threshold: float, stop_loss_ratio: float):
+    def __init__(self, portfolio: Portfolio, sell_threshold: float, buy_threshold: float, stop_loss_ratio: float, cash_ratio: float):
         self.portfolio = portfolio
         self.sell_threshold = sell_threshold
         self.buy_threshold = buy_threshold
         self.stop_loss_ratio = stop_loss_ratio
+        self.cash_ratio = cash_ratio
         self.target_cash = portfolio.initial_cash
 
     def backtest_strategy(self):
         self.portfolio.reset()
         stock_prices = {stock_name: stock.prices for stock_name, stock in self.portfolio.stocks.items()}
         previous_buy_or_sell_prices = {stock_name: stock_price[0] for stock_name, stock_price in stock_prices.items()}
-        for day in range(1, len(self.portfolio.stocks[next(iter(self.portfolio.stocks))].prices)):
+        days = range(1, len(self.portfolio.stocks[next(iter(self.portfolio.stocks))].prices))
+        for day in days:
             self._buy_or_sell_stocks(day, stock_prices, previous_buy_or_sell_prices)
 
     def _buy_or_sell_stocks(self, day, stock_prices, previous_buy_or_sell_prices):
@@ -82,11 +83,64 @@ class Investor:
             else:
                 change_from_previous_point = (current_price - previous_price) / previous_price
                 if change_from_previous_point <= -self.buy_threshold:
-                    n_stocks = floor(self.portfolio.cash / current_price)
+                    n_stocks = floor(self.portfolio.cash * self.cash_ratio / current_price)  # Considering cash_ratio
                     self.portfolio.cash -= n_stocks * current_price
                     self.portfolio.owned_stocks[stock_name] = n_stocks
                     previous_price = current_price
                     previous_buy_or_sell_prices[stock_name] = previous_price
+
+
+def eaSimpleWithElitism(population, toolbox, cxpb, mutpb, ngen, stats=None,
+             halloffame=None, verbose=__debug__):
+    logbook = tools.Logbook()
+    logbook.header = ['gen', 'nevals'] + (stats.fields if stats else [])
+
+    # Evaluate the individuals with an invalid fitness
+    invalid_ind = [ind for ind in population if not ind.fitness.valid]
+    fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+    for ind, fit in zip(invalid_ind, fitnesses):
+        ind.fitness.values = fit
+
+    if halloffame is not None:
+        halloffame.update(population)
+
+    record = stats.compile(population) if stats else {}
+    logbook.record(gen=0, nevals=len(invalid_ind), **record)
+    if verbose:
+        print(logbook.stream)
+
+    # Begin the generational process
+    for gen in range(1, ngen + 1):
+        # Select the next generation individuals
+        offspring = toolbox.select(population, len(population))
+
+        # Vary the pool of individuals
+        offspring = algorithms.varAnd(offspring, toolbox, cxpb, mutpb)
+
+        # Invalidate fitness of offspring
+        for ind in offspring:
+            del ind.fitness.values
+
+        # Evaluate the individuals with an invalid fitness
+        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+        fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+        for ind, fit in zip(invalid_ind, fitnesses):
+            ind.fitness.values = fit
+
+        # Update the hall of fame with the generated individuals
+        if halloffame is not None:
+            halloffame.update(offspring)
+
+        # Replace the current population by the offspring
+        population[:] = offspring
+
+        # Append the current generation statistics to the logbook
+        record = stats.compile(population) if stats else {}
+        logbook.record(gen=gen, nevals=len(invalid_ind), **record)
+        if verbose:
+            print(logbook.stream)
+
+    return population, logbook
 
 
 def evaluate(investor: Investor) -> Tuple[float]:
@@ -101,6 +155,8 @@ def mutate_investor(individual: Investor, mu: float, sigma: float, indpb: float)
         individual.buy_threshold = max(0, min(0.5, individual.buy_threshold + random.gauss(mu, sigma)))
     if random.random() < indpb:
         individual.stop_loss_ratio = max(0, min(0.5, individual.stop_loss_ratio + random.gauss(mu, sigma)))
+    if random.random() < indpb:
+        individual.cash_ratio = max(0, min(1, individual.cash_ratio + random.gauss(mu, sigma)))
 
     return individual,
 
@@ -109,6 +165,7 @@ def mate_investors(ind1: Investor, ind2: Investor):
     ind1.sell_threshold, ind2.sell_threshold = ind2.sell_threshold, ind1.sell_threshold
     ind1.buy_threshold, ind2.buy_threshold = ind2.buy_threshold, ind1.buy_threshold
     ind1.stop_loss_ratio, ind2.stop_loss_ratio = ind2.stop_loss_ratio, ind1.stop_loss_ratio
+    ind1.cash_ratio, ind2.cash_ratio = ind2.cash_ratio, ind1.cash_ratio
 
     return ind1, ind2
 
@@ -137,12 +194,14 @@ def main():
     toolbox.register("attr_sell", random.uniform, 0, 0.5)
     toolbox.register("attr_buy", random.uniform, 0, 0.5)
     toolbox.register("attr_stoploss", random.uniform, 0, 0.5)
+    toolbox.register("attr_cash_ratio", random.uniform, 0, 1)
 
     def generate_new_investor():
         return creator.Investor(Portfolio(stocks, initial_cash),
                                 sell_threshold=toolbox.attr_sell(),
                                 buy_threshold=toolbox.attr_buy(),
-                                stop_loss_ratio=toolbox.attr_stoploss())
+                                stop_loss_ratio=toolbox.attr_stoploss(),
+                                cash_ratio=toolbox.attr_cash_ratio())  # Include cash_ratio
 
     toolbox.register("investor", generate_new_investor)
 
@@ -160,19 +219,19 @@ def main():
     stats.register("min", np.min)
     stats.register("max", np.max)
 
-    pop, log = algorithms.eaSimple(pop, toolbox, cxpb=0.5, mutpb=0.2, ngen=generations, stats=stats, halloffame=hof, verbose=True)
+    try:
+        pop, log = eaSimpleWithElitism(pop, toolbox, cxpb=0.5, mutpb=0.2, ngen=generations, stats=stats, halloffame=hof, verbose=True)
+    except KeyboardInterrupt:
+        pass
 
-    print(f"Best individual is: sell_threshold={hof[0].sell_threshold:.3f}, "
-          f"buy_threshold={hof[0].buy_threshold:.3f}, "
-          f"stop_loss_ratio={hof[0].stop_loss_ratio:.3f}\n"
-          f"with fitness: {hof[0].fitness}")
-
-    for i in range(1, len(hof)):
-        individual = hof[i]
-        print(f"Individual {i} is: sell_threshold={individual.sell_threshold:.3f}, "
-              f"buy_threshold={individual.buy_threshold:.3f}, "
-              f"stop_loss_ratio={individual.stop_loss_ratio:.3f}\n"
-              f"with fitness: {individual.fitness}")
+    if hof:
+        for i in range(0, len(hof)):
+            individual = hof[i]
+            print(f"Individual {i+1} is: sell_threshold={individual.sell_threshold:.3f}, "
+                  f"buy_threshold={individual.buy_threshold:.3f}, "
+                  f"stop_loss_ratio={individual.stop_loss_ratio:.3f}, "
+                  f"cash_ratio={hof[i].cash_ratio:.3f}\n"
+                  f"with fitness: {individual.fitness}")
 
 
 if __name__ == '__main__':
